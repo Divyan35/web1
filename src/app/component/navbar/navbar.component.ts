9568d8d5d4014b38d8bfd9a66a25316acf841550
Divyan35 <55765667+Divyan35@users.noreply.github.com>
2020-01-22 07:07:00 +0530

    Update navbar.component.ts

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}]
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,private location: Location) { }
 
   ngOnInit() {
   }
+  
+  isHomeRouteActivated(): boolean{
+    //Find more better way to do it.
+    return this.location.path().indexOf('/who-we-are') > -1;
+}
 
 }
